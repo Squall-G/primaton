1355718f39a4fee47ce7ba228642b21623e07f29
gabriele.granelli <gabrielegranelli4@gmail.com>
2024-02-05 18:52:46 +0100

    FIX service

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -2,15 +2,12 @@ import { GetCurrent } from "../models/getCurrent";
 import { mock_current } from "./mock";
 
 export const fetchLocation = async (location: string): Promise<GetCurrent> => {
-  console.log(import.meta.env.VITE_MOCK);
   if (import.meta.env.VITE_MOCK === true) {
-    console.log(mock_current);
     return mock_current[Math.floor(Math.random() * 4)];
   }
+  const key = "b3c5b52569492b97a7678ed2e9f1ecd8";
   const response = await fetch(
-    `http://api.weatherstack.com/current?access_key=${
-      import.meta.env.VITE_KEY
-    }&query=${location}`
+    `http://api.weatherstack.com/current?access_key=${key}&query=${location}`
   );
   return response.json();
 };
